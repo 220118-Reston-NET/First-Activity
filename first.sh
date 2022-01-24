76526481f4f593117a627f6b81b66fdea9d48cd9
Your Name <you@example.com>
2022-01-23 22:20:13 -0500

    Shell_Calculator

--- first.sh
+++ first.sh
@@ -1,56 +1,50 @@
-#!/usr/bin/bash
-echo "=====Calculator ====="
-repeat="true"
-while [ "$repeat" == true ]
+# #!/usr/bin/bash
+# echo "=====Calculator ====="
+clear
+repeat=true
+while [ $repeat == true ]
 do
 echo "Enter two numbers"
 echo "Enter add if  you want to add two numbers"
 echo "Enter sub if  you want to subtract two numbers"
 echo "Enter mul if  you want to multiply two numbers"
 echo "Enter div if  you want to divide two numbers"
-echo "Enter ex to exit"
+echo "Enter any key to exit"
 read input
 
-if [ "$input" == "add" ]
+if [ $input == "add" ]
+then
+    echo "Enter first number"
+    read num1
+    echo "Enter second number"
+    read num2
+    echo " $num1 + $num2 = $(($num1 + $num2))"
+elif [ $input == "sub" ]
+then
+    echo "Enter first number"
+    read num1
+    echo "Enter second number"
+    read num2
+    echo " $num1 - $num2 = $(($num1 - $num2))"
+elif [ $input == "mul" ]
+then
+    echo "Enter first number"
+    read num1
+    echo "Enter second number"
+    read num2
+    echo " $num1 * $num2 = $(($num1 * $num2))"
+
+elif [ $input == "div" ]
 then
 echo "Enter first number"
 read num1
 echo "Enter second number"
 read num2
-echo " your added Result is $(($num1 + $num2))"
-
-read input
-elif [ "$input" == "sub" ]
-then
-echo "Enter first number"
-read num1
-echo "Enter second number"
-read num2
-echo " your subtracted Result is $(($num1 - $num2))"
-
-read input
-elif [ "$input" == "mul" ]
-then
-echo "Enter first number"
-read num1
-echo "Enter second number"
-read num2
-echo " your Multiplied  Result is $(($num1 * $num2))"
-
-read input
-elif [ "$input" == "div" ]
-then
-echo "Enter first number"
-read num1
-echo "Enter second number"
-read num2
-echo " your divided  Result is $(($num1 / $num2))"
+echo " $num1 / $num2 = $(($num1 / $num2))"
 else
-repeat="false"
-echo "Error"
+repeat=false
+echo "====Thank you for using this Calaculator"
 
 fi
 
-clear
 done
-
